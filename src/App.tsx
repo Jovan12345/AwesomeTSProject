5805741aa194039440fd5616e4498b9688a91583
Jovan12345 <jovanj@gecko.mk>
2020-12-03 17:42:27 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello from react native for web</Text>
+      <Text>Dupi tamu</Text>
+      <Text>Da mu ebam majkata</Text>
+    </View>
+  );
+};
+
+export default App;
